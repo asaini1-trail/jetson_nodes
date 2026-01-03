e4a128ac54822c125e27ca8146f68479a53b99db
sainia1 <sainiabhijeet11@gmail.com>
2026-01-02 22:00:30 -0500

    Remove build, install, and log directories from repository

--- build/ros_test/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/abhijeet/home/abhijeet/ros2_ws/src/ros_test
-# Build directory: /home/abhijeet/home/abhijeet/ros2_ws/src/build/ros_test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/rover_sw_pwrtrain/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/abhijeet/home/abhijeet/ros2_ws/src/rover_sw_pwrtrain
-# Build directory: /home/abhijeet/home/abhijeet/ros2_ws/src/build/rover_sw_pwrtrain
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
